d3110033e6d00a13c40538cee7dda71f4612ed7b
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-02-08 09:42:49 +0100

    removed hard coded IP

--- src/main/resources/static/frontend/app/scripts/app/app.config.js
+++ src/main/resources/static/frontend/app/scripts/app/app.config.js
@@ -1,8 +1,8 @@
 angular.module('app.config', [])
 
-.constant('backendUrl', 'http://155.105.202.24:8080')
+.constant('backendUrl', 'http://155.105.202.25:8080')
 
-.constant('backendExportChartUrl', 'http://155.105.202.24:8080/exportingChart.php')
+.constant('backendExportChartUrl', 'http://155.105.202.25:8080/exportingChart.php')
 
 .constant('dropboxAppkey', '7wew0rj0gh2qcik')
 
