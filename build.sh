7c3c1a288cef6a8935fbf55a85aa585c4d075616
Ludovic Claude <ludovic.claude54@gmail.com>
2016-02-15 11:57:07 +0100

    Use the Maven repository on the host to avoid downloading Internet again

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ else
   DOCKER_COMPOSE="sudo docker-compose"
 fi
 
+[ -e .m2 ] || ln -s .m2 ~/.m2
+
 $DOCKER_COMPOSE --file=docker-compose.init.yml --project-name=portal-backend-init up
 
 echo "Need to set the current user as owner of the files generated in target directory..."
