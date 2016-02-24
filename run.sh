0b25bc3967d88ad967cb4ea683bb83531a610d6d
Ludovic Claude <ludovic.claude54@gmail.com>
2016-02-24 12:38:44 +0100

    run runs, build builds

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -e
 
+# Run the backend in a Docker container and start the database.
+# The current project is compiler inside the Docker container
+
 if groups $USER | grep &>/dev/null '\bdocker\b'; then
   DOCKER_COMPOSE="docker-compose"
 else
   DOCKER_COMPOSE="sudo docker-compose"
 fi
 
-$DOCKER_COMPOSE up
+# Create a symlink to the local Maven repository
+[ -L .m2 ] || ln -s -t . ~/.m2
+
+$DOCKER_COMPOSE --file=docker-compose.init.yml --project-name=portal-backend-init up
+
+echo "Need to set the current user as owner of the files generated in target directory..."
+sudo chown -R $USER:$USER ./target
+
+echo "Done"
