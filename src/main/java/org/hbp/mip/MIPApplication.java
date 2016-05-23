2ead7745a613baffcbf9b0eb5e7d41e16726d220
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-05-23 15:50:50 +0200

    Bugfix 403 on auto-logout at startup

--- src/main/java/org/hbp/mip/MIPApplication.java
+++ src/main/java/org/hbp/mip/MIPApplication.java
@@ -225,6 +225,7 @@ public class MIPApplication extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().exceptionHandling().authenticationEntryPoint(new CustomLoginUrlAuthenticationEntryPoint("/login/hbp"))
                 .and().logout().logoutSuccessUrl("/login/hbp").permitAll()
+                .and().logout().permitAll()
                 .and().csrf().csrfTokenRepository(csrfTokenRepository())
                 .and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
                 .addFilterBefore(hbpFilter, BasicAuthenticationFilter.class);
