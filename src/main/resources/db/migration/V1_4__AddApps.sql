fcac264269cc42ba410e75e799174da19b79754a
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-06-16 10:06:35 +0200

    add 2D Biological Rules app external link

--- src/main/resources/db/migration/V1_4__AddApps.sql
+++ src/main/resources/db/migration/V1_4__AddApps.sql
@@ -4,7 +4,7 @@
 
 COPY app (id, author, category, description, email, image, link, name, ratingcount, totalrating) FROM stdin;
 1	Linda Dib and Mirco Nasuti	Data Mining Tools	Choose a brain region, a gene and a time-frame to visualize gene expression and find correlations betweeen them.	mirco.nasuti@gmail.com	https://mip.humanbrainproject.eu/images/apps/logo-geneexpression.png	https://mip.humanbrainproject.eu/genexpression	Gene Expression	0	0
-2	Unknown	Data Mining Tools	Navigate in a 3D world of variables.	\N	https://mip.humanbrainproject.eu/images/apps/logo-graphviewer.png	https://mip.humanbrainproject.eu/scripts/external/ViewerProject/GraphViewer.html	Biological Rules	0	0
+2	Unknown	Data Mining Tools	Navigate in a 3D world of variables.	\N	https://mip.humanbrainproject.eu/images/apps/logo-3d-explore.png	https://mip.humanbrainproject.eu/scripts/external/ViewerProject/GraphViewer.html	3D Biological Rules	0	0
 3	Unknown	Data Mining Tools	Categorize, cluster and classify data.	\N	https://mip.humanbrainproject.eu/images/apps/logo-3c.png	https://mip.humanbrainproject.eu/scripts/external/3c/index.html	3C	0	0
 4	Unknown	Data Exploration Tools	Visualize medical data using bar graphs.	\N	https://mip.humanbrainproject.eu/images/apps/logo-bargraph.png	https://mip.humanbrainproject.eu/graphdata/bargraph.html	Bargraph	0	0
 5	Unknown	Data Exploration Tools	Visualize medical data using sunburst graphs.	\N	https://mip.humanbrainproject.eu/images/apps/logo-sunburst.png	https://mip.humanbrainproject.eu/graphdata/sunburst.html	Sunburst	0	0
---
+++ src/main/resources/db/migration/V2_0__AddBioRulesApp.sql
@@ -0,0 +1,4 @@
+INSERT INTO app (id, author, category, description, email, image, link, name, ratingcount, totalrating)
+VALUES (8, 'Mirco Nasuti', 'Data Mining Tools', '2D graphical view of biological rules', 'mirco.nasuti@chuv.ch',
+'https://mip.humanbrainproject.eu/images/apps/logo-graphviewer.png', 'http://hbps1.chuv.ch/biorules/app',
+'2D Biological Rules', 0, 0)
