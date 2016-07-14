a8a9a0d7d673855eedcc450df522346ca69d2c03
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-07-14 14:10:23 +0200

    wait for db ready before starting backend

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 docker-compose build
-docker-compose up -d
+docker-compose up -d db
+
+echo 'Waiting for database to be ready...'
+until [ $(docker-compose exec db psql -U postgres -c "\q" | wc -l) -eq 0 ]; do
+    printf '.'
+    sleep 1
+done
+
+docker-compose up -d backend
