83dc18a820e010b1277f5c0c1ea2df485c35db40
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-07-18 14:00:58 +0200

    bugfix run

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ docker-compose build
 docker-compose up -d db
 
 echo 'Waiting for database to be ready...'
-until [ $(docker-compose exec db psql -U postgres -c "\q" | wc -l) -eq 0 ]; do
+until [ $(docker-compose exec portal-db psql -U postgres -c "\q" | wc -l) -eq 0 ]; do
     printf '.'
     sleep 1
 done
