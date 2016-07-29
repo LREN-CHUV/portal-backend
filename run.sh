8fe7b7e58d6c7f507103ed8935dc07663f824aed
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-07-29 14:09:33 +0200

    upgrade several dependencies and a few other changes

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 docker-compose build
 docker-compose up -d portal-db
 
-echo 'Waiting for database to be ready...'
+echo "Waiting for database to be ready..."
 until [ $(docker-compose exec portal-db psql -U postgres -c "\q" 2>&1 | wc -l) -eq 0 ]; do
     printf '.'
     sleep 1
