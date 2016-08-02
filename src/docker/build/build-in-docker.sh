231e8e2f5f697f2972882049969df75afcc7023e
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-08-02 13:05:12 +0200

    run maven sonar plugin at build time

--- src/docker/build/build-in-docker.sh
+++ src/docker/build/build-in-docker.sh
@@ -6,8 +6,8 @@ rm -rf target/
 echo "Packaging..."
 mvn package
 
-# echo "Adding HBPS1 certificate to key store..."
-# yes | keytool -import -trustcacerts -alias hbps1.chuv.ch -file /hbps1.crt -storepass 'changeit' -keystore $JAVA_HOME/jre/lib/security/cacerts
+echo "Adding HBPS1 certificate to key store..."
+yes | keytool -import -trustcacerts -alias hbps1.chuv.ch -file /hbps1.crt -storepass 'changeit' -keystore $JAVA_HOME/jre/lib/security/cacerts
 
-# echo "Running Sonar analysis"
-# mvn sonar:sonar
+echo "Running Sonar analysis"
+mvn sonar:sonar
