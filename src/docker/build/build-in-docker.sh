d8fba12b7cabd7b42540dd4f049877624835ed79
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-08-09 14:44:09 +0200

    removed sonar related stuff

--- src/docker/build/build-in-docker.sh
+++ src/docker/build/build-in-docker.sh
@@ -5,9 +5,3 @@ rm -rf target/
 
 echo "Packaging..."
 mvn package
-
-echo "Adding HBPS1 certificate to key store..."
-yes | keytool -import -trustcacerts -alias hbps1.chuv.ch -file /hbps1.crt -storepass 'changeit' -keystore $JAVA_HOME/jre/lib/security/cacerts
-
-echo "Running Sonar analysis"
-mvn sonar:sonar -Dsonar.login=892200bfa760a89cfc67c5ee7d86a7a34a5ba3aa
