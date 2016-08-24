118b6bf6d88c83c1034dbc3776658689fa0e821a
Ludovic Claude <ludovic.claude54@gmail.com>
2016-08-24 18:44:52 +0200

    I said on push

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
 
 export WORKSPACE=$(pwd)
 captain push --branch-tags=false --commit-tags=true portal-backend
+curl -k -X POST --data-urlencode payload@$WORKSPACE/src/docker/run/slack.json https://hbps1.chuv.ch/slack/dev-activity
