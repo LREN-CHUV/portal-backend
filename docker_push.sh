1ac197ab11033813dceb9f6ca158208213d7dc5a
Ludovic Claude <ludovic.claude54@gmail.com>
2016-08-24 18:47:41 +0200

    Better workspace location detection

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-export WORKSPACE=$(pwd)
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     cd -P "$( dirname "$SOURCE" )"
+     pwd
+}
+
+export WORKSPACE=$(get_script_dir)
 captain push --branch-tags=false --commit-tags=true portal-backend
 curl -k -X POST --data-urlencode payload@$WORKSPACE/src/docker/run/slack.json https://hbps1.chuv.ch/slack/dev-activity
