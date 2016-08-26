8db89e02e125d6f32504699b3a601375cfb8caa8
Ludovic Claude <ludovic.claude54@gmail.com>
2016-08-26 19:45:09 +0200

    Fix wait for db

--- docker/runner/run.sh
+++ docker/runner/run.sh
@@ -2,6 +2,6 @@
 
 OPTS="-template /config/application.tmpl:/config/application.yml"
 if [ ! -z "$DB_SERVER" ]; then
-  OPTS="$OPTS -wait $DB_SERVER -timeout 60s"
+  OPTS="$OPTS -wait tcp://$DB_SERVER -timeout 60s"
 fi
 dockerize $OPTS java -jar backend.jar
