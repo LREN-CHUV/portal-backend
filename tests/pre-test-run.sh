06baffa0b30a9d4e91f6e67c3492506f3e0863e0
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-09-02 10:35:22 +0200

    remove outdated integration tests

--- tests/pre-test-run.sh
+++ tests/pre-test-run.sh
@@ -14,7 +14,7 @@ if [ $(docker ps -a | grep portal-backend-test | wc -l) -gt 0 ]; then
   docker rm -f portal-backend-test
 fi
 
-docker run --name portal-db-test -p 5432:5432 -e POSTGRES_USER=postgres -d postgres
+docker run --name portal-db-test -p 5432:5432 -e POSTGRES_USER=postgres -d postgres:9.5.3
 
 echo "Waiting for DB to start..."
 until [ $(docker exec portal-db-test psql -U postgres -c "\q" 2>&1 | wc -l) -eq 0 ]; do
@@ -26,7 +26,8 @@ GATEWAY_IP=$(docker inspect portal-db-test | grep \"Gateway\":\ \" | sed 's/.*Ga
 
 docker run --name portal-backend-test -p 8080:8080 \
 -e "AUTHENTICATION=0" \
--e "DB_URL=jdbc:postgresql://$GATEWAY_IP:5432/postgres" \
+-e "DB_URL=jdbc:postgresql://$GATEWAY_IP:5433/postgres" \
+-e "META_DB_URL=jdbc:postgresql://$GATEWAY_IP:5432/postgres" \
 -d hbpmip/portal-backend
 
 echo "Waiting for backend to start..."
