08bf5961175bb4cb5be671aac3eccbfa63ac064a
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-09-12 09:56:40 +0200

    fix env variable name in run.sh

--- docker/runner/run.sh
+++ docker/runner/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 OPTS="-template /config/application.tmpl:/config/application.yml"
-if [ ! -z "$DB_SERVER" ]; then
-  OPTS="$OPTS -wait tcp://$DB_SERVER -timeout 60s"
+if [ ! -z "$PORTAL_DB_URL" ]; then
+  OPTS="$OPTS -wait tcp://$PORTAL_DB_URL -timeout 60s"
 fi
 if [ ! -z "$META_DB_SERVER" ]; then
   OPTS="$OPTS -wait tcp://$META_DB_SERVER -timeout 60s"
