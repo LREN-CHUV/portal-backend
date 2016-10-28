7de23619f442b848e67a586dd3e61e13108d7270
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-10-28 15:24:14 +0200

    fix db migration add filters

--- src/main/resources/db/migration/V3_0__AddFilter.sql
+++ src/main/resources/db/migration/V3_0__AddFilter.sql
@@ -3,6 +3,6 @@ ALTER TABLE query ADD COLUMN sql_filter text;
 ALTER TABLE query_filter DROP COLUMN filters_id;
 DROP TABLE filter_values, filter;
 
-ALTER TABLE query_filter ADD COLUMN code character varying(255) NOT NULL;
+ALTER TABLE query_filter ADD COLUMN code character varying(255) NOT NULL DEFAULT '';
 ALTER TABLE ONLY query_filter
     ADD CONSTRAINT fk_1 FOREIGN KEY (query_id) REFERENCES query(id);
