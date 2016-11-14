4890a86af37ef97db786de3984fb3f479d70826a
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-11-14 13:33:40 +0100

    DB migration : use only local paths to apps

---
+++ src/main/resources/db/migration/V4_2__OnlyLocalAppsPaths.sql
@@ -0,0 +1,5 @@
+UPDATE app SET link='/GenExpression/index.html' WHERE id=1;
+UPDATE app SET link='/Bargraph/index.html' WHERE id=4;
+UPDATE app SET link='/Sunburst/index.html' WHERE id=5;
+UPDATE app SET link='/3d-brain/index.html' WHERE id=6;
+UPDATE app SET link='/2d-bio-rules/app/index.html' WHERE id=8;
