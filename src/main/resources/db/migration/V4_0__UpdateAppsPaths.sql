1d4848b663c084f028ceb2d75cacc2d4429801b9
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-11-14 11:29:01 +0100

    DB migration : Use relative paths for apps

---
+++ src/main/resources/db/migration/V4_0__UpdateAppsPaths.sql
@@ -0,0 +1,12 @@
+UPDATE app SET link='/ViewerProject/GraphViewer.html' WHERE id=2;
+UPDATE app SET link='/3c/index.html' WHERE id=3;
+UPDATE app SET link='/Papaya/standard-with-atlas/index.html' WHERE id=7;
+
+UPDATE app SET image='/logo-geneexpression.png' WHERE id=1;
+UPDATE app SET image='/logo-3d-explore.png' WHERE id=2;
+UPDATE app SET image='/logo-3c.png' WHERE id=3;
+UPDATE app SET image='/logo-bargraph.png' WHERE id=4;
+UPDATE app SET image='/logo-sunburst.png' WHERE id=5;
+UPDATE app SET image='/logo-3d-brain.png' WHERE id=6;
+UPDATE app SET image='/logo-2d-atlas.png' WHERE id=7;
+UPDATE app SET image='/logo-graphviewer.png' WHERE id=8;
