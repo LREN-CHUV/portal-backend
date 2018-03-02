e1bf8a112907135cd7ec4617cbe262ec43b4005c
Ludovic Claude <ludovic.claude54@gmail.com>
2018-03-02 14:28:37 +0100

    Update woken-message to 2.4.9, Fix read models with filters

--- src/main/scala/ch.chuv.lren/mip/portal/WokenConversions.scala
+++ src/main/scala/ch.chuv.lren/mip/portal/WokenConversions.scala
@@ -3,6 +3,7 @@ package ch.chuv.lren.mip.portal
 import eu.hbp.mip.model.Variable
 import ch.chuv.lren.woken.messages.datasets.DatasetId
 import ch.chuv.lren.woken.messages.query.filters.FilterRule
+import ch.chuv.lren.woken.messages.query.filters.FilterRule._
 import ch.chuv.lren.woken.messages.query.filters.queryFiltersProtocol._
 import org.springframework.stereotype.Component
 import spray.json._
@@ -19,5 +20,7 @@ class WokenConversions {
 
   def toDatasets(datasets: java.util.List[Variable]): Set[DatasetId] =
     datasets.asScala.map(v => DatasetId(v.getCode)).toSet
+
+  def toSqlWhere(filter: Option[FilterRule]): String = filter.fold("")(_.toSqlWhere)
 
 }
