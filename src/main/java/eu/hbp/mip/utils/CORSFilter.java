6801700167dba0bdf792372e9c51ea95a8ebf6f8
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-02-14 22:19:09 +0100

    Added PUT to CORS

--- src/main/java/eu/hbp/mip/utils/CORSFilter.java
+++ src/main/java/eu/hbp/mip/utils/CORSFilter.java
@@ -13,9 +13,10 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Request-Headers", "*");
         chain.doFilter(req, res);
     }
 
