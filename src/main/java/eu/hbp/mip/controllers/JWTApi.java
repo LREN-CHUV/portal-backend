422a36f7424879a84cf8a457a2dd1b844c3e8753
Manuel Spuhler <manuel.spuhler@gmail.com>
2019-10-07 15:59:06 +0200

    JWT wip

--- src/main/java/eu/hbp/mip/controllers/JWTApi.java
+++ src/main/java/eu/hbp/mip/controllers/JWTApi.java
@@ -7,7 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.JWT;
-
+import eu.hbp.mip.model.User;
+import eu.hbp.mip.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -19,17 +22,25 @@ public class JWTApi {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JWTApi.class);
 
+    @Autowired
+    private UserInfo userInfo;
+
+    @Value("#{'${services.workflows.JWTSecret}'}")
+    private String JWTSecret;
+
     @ApiOperation(value = "Create a JSON Web Token", response = String.class)
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<String> createJWT() {
 
         LOGGER.info("Create a JSON Web Token");
 
-        Algorithm algorithm = Algorithm.HMAC512("secret");
-        String token = JWT.create()
-        .withIssuer("mip.humanbrainproject.eu")
-        .withSubject("subj")
-        .sign(algorithm);
+        User user = userInfo.getUser();
+
+        Algorithm algorithm = Algorithm.HMAC512(JWTSecret);
+        String token = JWT.create().withIssuer("mip.humanbrainproject.eu").withSubject(user.getEmail()).sign(algorithm);
+
+        LOGGER.info(algorithm.toString());
+        LOGGER.info(token);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(token);
     }
