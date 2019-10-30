83dbea15b127512ebff51bc170ae0a3471d19736
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-10-30 19:23:37 +0100

    Cleanup

--- src/main/java/eu/hbp/mip/controllers/MethodsApi.java
+++ src/main/java/eu/hbp/mip/controllers/MethodsApi.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import eu.hbp.mip.model.User;
